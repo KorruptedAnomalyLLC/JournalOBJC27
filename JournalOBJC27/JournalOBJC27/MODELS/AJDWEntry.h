0720420def9135872cd61912359d1b16b9aa3c9a
Austin West <korruptedanomaly@gmail.com>
2019-07-02 09:12:28 -0600

    Last update

---
+++ JournalOBJC27/JournalOBJC27/MODELS/AJDWEntry.h
@@ -0,0 +1,17 @@
+//
+//  AJDWEntry.h
+//  JournalOBJC27
+//
+//  Created by Austin West on 7/2/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AJDWEntry : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JournalOBJC27/JournalOBJC27/MODELS/AJDWEntry.m
@@ -0,0 +1,13 @@
+//
+//  AJDWEntry.m
+//  JournalOBJC27
+//
+//  Created by Austin West on 7/2/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import "AJDWEntry.h"
+
+@implementation AJDWEntry
+
+@end
